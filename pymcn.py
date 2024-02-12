2ef55ef436b5c6fce1c46f2d047d99a924ea7c7f
wcollins <26700607+wcollins@users.noreply.github.com>
2024-02-12 17:54:17 -0500

    Update parameters

--- pymcn.py
+++ pymcn.py
@@ -3,110 +3,123 @@ import os
 import boto3
 import pandas as pd
 from netaddr import IPNetwork
+import uuid
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.network.models import VirtualNetwork, Subnet
 from google.cloud import compute_v1
 from google.auth import exceptions
+from google.api_core import exceptions as google_exceptions
 
 class CloudManager:
-    '''
-    Class that manages base networking for AWS, Azure, and GCP
-    '''
     def __init__(self, df, filename):
+        print("Initializing CloudManager...")
         self.df = df
         self.filename = filename
+        if 'route_table_id' not in df.columns:
+            self.df['route_table_id'] = pd.NA
+            print("Added 'route_table_id' column to DataFrame.")
 
     def create_aws_vpcs(self):
-        '''
-        Iterate over each row in the dataframe and create VPC if cloud == aws
-        '''
+        print("Starting AWS VPC creation...")
         for index, row in self.df.iterrows():
-            if row['cloud'] == 'aws' and pd.isnull(row['resource_id']):
+            if row['cloud'] == 'aws' and pd.isnull(row['network_id']):
+                print(f"Creating AWS VPC in region {row['region']} with CIDR {row['cidr']}")
                 session = boto3.Session(region_name=row['region'])
                 ec2_resource = session.resource('ec2')
                 vpc = ec2_resource.create_vpc(CidrBlock=row['cidr'])
                 vpc.create_tags(Tags=[{"Key": "Name", "Value": row['name']}])
                 vpc.wait_until_available()
-                self.df.at[index, 'resource_id'] = vpc.id
+                self.df.at[index, 'network_id'] = vpc.id
+                print(f"Created VPC {vpc.id}")
+
+                main_route_table = list(vpc.route_tables.all())[0]
+                self.df.at[index, 'route_table_id'] = main_route_table.id
 
                 networks = list(IPNetwork(row['cidr']).subnet(24))
 
                 for i in range(int(row['num_subnets'])):
-                    subnet = ec2_resource.create_subnet(VpcId=vpc.id, CidrBlock=str(networks[i]))
+                    subnet_name = f"subnet-{uuid.uuid4()}"
+                    print(f"Creating subnet {i+1}/{int(row['num_subnets'])}: {subnet_name}")
+                    subnet = ec2_resource.create_subnet(VpcId=vpc.id, CidrBlock=str(networks[i]), TagSpecifications=[{'ResourceType': 'subnet', 'Tags': [{'Key': 'Name', 'Value': subnet_name}]}])
 
     def delete_aws_vpcs(self):
-        '''
-        Iterate over each row in the dataframe and delete VPC if cloud == aws and 'resource_id' is not null
-        '''
+        print("Starting AWS VPC deletion...")
         for index, row in self.df.iterrows():
-            if row['cloud'] == 'aws' and not pd.isnull(row['resource_id']):
+            if row['cloud'] == 'aws' and not pd.isnull(row['network_id']):
+                print(f"Deleting AWS VPC {row['network_id']}")
                 session = boto3.Session(region_name=row['region'])
                 ec2_resource = session.resource('ec2')
-                vpc = ec2_resource.Vpc(row['resource_id'])
+                vpc = ec2_resource.Vpc(row['network_id'])
 
                 for subnet in vpc.subnets.all():
+                    print(f"Deleting subnet {subnet.id}")
                     subnet.delete()
 
                 for route_table in vpc.route_tables.all():
                     if not route_table.associations_attribute:
+                        print(f"Deleting route table {route_table.id}")
                         route_table.delete()
 
                 for network_acl in vpc.network_acls.all():
                     if not network_acl.is_default:
+                        print(f"Deleting network ACL {network_acl.id}")
                         network_acl.delete()
 
                 for security_group in vpc.security_groups.all():
                     if security_group.group_name != 'default':
+                        print(f"Deleting security group {security_group.id}")
                         security_group.delete()
 
+                print(f"Deleting VPC {vpc.id}")
                 vpc.delete()
-                self.df.at[index, 'resource_id'] = pd.NA
+                self.df.at[index, 'network_id'] = pd.NA
+                self.df.at[index, 'route_table_id'] = pd.NA
 
     def create_azure_vnets(self):
-        '''
-        Iterate over each row in the dataframe and create VNet if cloud == azure
-        '''
+        print("Starting Azure VNet creation...")
         credential = DefaultAzureCredential()
         subscription_id = os.getenv('SUBSCRIPTION_ID')
 
         for index, row in self.df.iterrows():
-            if row['cloud'] == 'azure' and pd.isnull(row['resource_id']):
+            if row['cloud'] == 'azure' and pd.isnull(row['network_id']):
+                print(f"Creating Azure VNet in resource group {row['resource_group']} with CIDR {row['cidr']}")
                 network_client = NetworkManagementClient(credential, subscription_id)
 
                 vnet_params = VirtualNetwork(location=row['region'], address_space={'address_prefixes': [row['cidr']]})
                 async_vnet_creation = network_client.virtual_networks.begin_create_or_update(row['resource_group'], row['name'], vnet_params)
                 vnet = async_vnet_creation.result()
 
-                self.df.at[index, 'resource_id'] = vnet.id
+                self.df.at[index, 'network_id'] = vnet.id
+                print(f"Created VNet {vnet.id}")
 
                 networks = list(IPNetwork(row['cidr']).subnet(24))
 
                 for i in range(int(row['num_subnets'])):
-                    subnet_params = Subnet(address_prefix=str(networks[i]))
-                    network_client.subnets.begin_create_or_update(row['resource_group'], vnet.name, 'subnet' + str(i), subnet_params)
+                    subnet_name = f"subnet-{uuid.uuid4()}"
+                    print(f"Creating subnet {i+1}/{int(row['num_subnets'])}: {subnet_name}")
+                    subnet_params = Subnet(address_prefix=str(networks[i]), name=subnet_name)
+                    network_client.subnets.begin_create_or_update(row['resource_group'], vnet.name, subnet_name, subnet_params)
 
     def delete_azure_vnets(self):
-        '''
-        Iterate over each row in the dataframe and delete VNet if cloud == azure and 'resource_id' is not null
-        '''
+        print("Starting Azure VNet deletion...")
         credential = DefaultAzureCredential()
         subscription_id = os.getenv('SUBSCRIPTION_ID')
 
         for index, row in self.df.iterrows():
-            if row['cloud'] == 'azure' and not pd.isnull(row['resource_id']):
+            if row['cloud'] == 'azure' and not pd.isnull(row['network_id']):
+                print(f"Deleting Azure VNet {row['name']} in resource group {row['resource_group']}")
                 network_client = NetworkManagementClient(credential, subscription_id)
 
                 async_vnet_deletion = network_client.virtual_networks.begin_delete(row['resource_group'], row['name'])
                 async_vnet_deletion.wait()
-                self.df.at[index, 'resource_id'] = pd.NA
+                self.df.at[index, 'network_id'] = pd.NA
 
     def create_gcp_vpcs(self):
-        '''
-        Iterate over each row in the dataframe and create VPC if cloud == gcp
-        '''
+        print("Starting GCP VPC creation...")
         for index, row in self.df.iterrows():
-            if row['cloud'] == 'gcp' and pd.isnull(row['resource_id']):
+            if row['cloud'] == 'gcp' and pd.isnull(row['network_id']):
+                print(f"Creating GCP VPC in project {row['project_id']} with name {row['name']}")
                 project_id = row['project_id']
                 client = compute_v1.NetworksClient()
 
@@ -119,19 +132,21 @@ class CloudManager:
                     operation = client.insert(project=project_id, network_resource=network)
                     operation.result()
 
-                    # Get the created network
                     network = client.get(project=project_id, network=row['name'])
                     network_id = network.self_link.split('/')[-1]
 
-                    self.df.at[index, 'resource_id'] = network_id
+                    self.df.at[index, 'network_id'] = network_id
+                    print(f"Created VPC {network_id}")
 
                     networks = list(IPNetwork(row['cidr']).subnet(24))
 
                     subnetwork_client = compute_v1.SubnetworksClient()
 
                     for i in range(int(row['num_subnets'])):
+                        subnet_name = f"subnet-{uuid.uuid4()}"
+                        print(f"Creating subnet {i+1}/{int(row['num_subnets'])}: {subnet_name}")
                         subnet = compute_v1.Subnetwork(
-                            name='subnet' + str(i),
+                            name=subnet_name,
                             ip_cidr_range=str(networks[i]),
                             region=row['region'],
                             network=network.self_link
@@ -143,71 +158,67 @@ class CloudManager:
                     print(f"Failed to create VPC in GCP for project {project_id}. Error: {e}")
 
     def delete_gcp_vpcs(self):
-        '''
-        Iterate over each row in the dataframe and delete VPC if cloud == gcp and 'resource_id' is not null
-        '''
+        print("Starting GCP VPC deletion...")
         for index, row in self.df.iterrows():
-            if row['cloud'] == 'gcp' and not pd.isnull(row['resource_id']):
+            if row['cloud'] == 'gcp' and not pd.isnull(row['network_id']):
+                # Move the assignment of project_id here, before it's used
                 project_id = row['project_id']
+                print(f"Attempting to delete GCP VPC {row['name']} in project {project_id}")
                 client = compute_v1.NetworksClient()
 
-                # Get network
-                network = client.get(project=project_id, network=row['name'])
+                try:
+                    regions_client = compute_v1.RegionsClient()
+                    regions = regions_client.list(project=project_id)
+                    subnetworks_client = compute_v1.SubnetworksClient()
 
-                # Get list of subnets
-                subnetworks_client = compute_v1.SubnetworksClient()
-                aggregated_list = subnetworks_client.aggregated_list(project=project_id)
-
-                for region_uri, subnetworks_scoped_list in aggregated_list:
-                    if subnetworks_scoped_list.subnetworks:
-                        for subnetwork in subnetworks_scoped_list.subnetworks:
-                            if subnetwork.network == network.self_link:
-
-                                # Fetch region name from the region URI
-                                region = region_uri.split('/')[-1]
-
-                                # Delete the subnet
-                                operation = subnetworks_client.delete(project=project_id, region=region, subnetwork=subnetwork.name)
+                    for region in regions:
+                        subnets = subnetworks_client.list(project=project_id, region=region.name)
+                        for subnet in subnets:
+                            if subnet.network.endswith(row['name']):
+                                print(f"Deleting subnet {subnet.name} in network {row['name']} and region {region.name}")
+                                operation = subnetworks_client.delete(project=project_id, region=region.name, subnetwork=subnet.name)
                                 operation.result()
 
-                # Delete VPC
-                operation = client.delete(project=project_id, network=row['name'])
-                operation.result()
+                    print(f"Deleting VPC {row['name']}")
+                    operation = client.delete(project=project_id, network=row['name'])
+                    operation.result()
+                    self.df.at[index, 'network_id'] = pd.NA
 
-                self.df.at[index, 'resource_id'] = pd.NA
+                except google_exceptions.NotFound:
+                    print(f"Network {row['name']} not found in project {project_id}.")
+                except google_exceptions.BadRequest as e:
+                    print(f"BadRequest error when deleting network {row['name']} in project {project_id}: {e}")
+                except Exception as e:
+                    print(f"Unexpected error: {e}")
 
     def process_networks(self, delete_flag=None):
-        '''
-        Logic to call either create or delete methods based on --delete flag
-        '''
-
-        # Is delete flag?
         if delete_flag == "--delete":
+            print("Deleting networks based on DataFrame...")
             self.delete_aws_vpcs()
             self.delete_azure_vnets()
             self.delete_gcp_vpcs()
-
-        # Is not delete flag?
         else:
+            print("Creating networks based on DataFrame...")
             self.create_aws_vpcs()
             self.create_azure_vnets()
             self.create_gcp_vpcs()
 
+        print(f"Saving DataFrame to {self.filename}")
         self.df.to_excel(self.filename, index=False)
-
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 pymcn.py <networks.xlsx> [--delete]")
+        print("Usage: python3 script.py <networks.xlsx> [--delete]")
         sys.exit(1)
 
     filename = sys.argv[1]
     delete_flag = sys.argv[2] if len(sys.argv) > 2 else None
     df = pd.read_excel(filename)
 
+    print("Loaded DataFrame from file.")
     manager = CloudManager(df, filename)
     manager.process_networks(delete_flag)
-
+    print("Operation completed.")
 
 if __name__ == "__main__":
     main()
